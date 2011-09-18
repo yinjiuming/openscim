63eceb4db7f75a0f53fc544fc4b4a5e8c5143ebe
matthewcrooke <matthewcrooke@gmail.com>
2011-09-18 09:28:02 +0000

    Adding mapping of ldap groupOfNames to scim object model

--- java/openscim-restful-server/src/main/java/openscim/restful/server/resources/group/ldap/GroupAttributesMapper.java
+++ java/openscim-restful-server/src/main/java/openscim/restful/server/resources/group/ldap/GroupAttributesMapper.java
@@ -18,15 +18,81 @@
  */
 package openscim.restful.server.resources.group.ldap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
+
+import openscim.entities.Group;
+import openscim.entities.PluralAttribute;
+import openscim.entities.User;
+import openscim.entities.User.Emails;
+import openscim.restful.server.resources.util.ResourceUtilities;
 
 import org.springframework.ldap.core.AttributesMapper;
 
 public class GroupAttributesMapper implements AttributesMapper
 {
+	public static final String ATTRIBUTE_PREFIX = "attributes.groups.";
+	public static final String GROUP_OBJECTCLASS_ATTRIBUTE = ATTRIBUTE_PREFIX + "objectclass";
+	public static final String GID_ATTRIBUTE = ATTRIBUTE_PREFIX + "cn";
+	public static final String MEMBER_ATTRIBUTE = ATTRIBUTE_PREFIX + "member";	
+	public static final String DEFAULT_GROUP_OBJECTCLASS_ATTRIBUTE = "groupOfNames";
+	public static final String DEFAULT_GID_ATTRIBUTE = "cn";
+	public static final String DEFAULT_MEMBER_ATTRIBUTE = "member";
+	
+	private Properties properties = null;
+	
+	public GroupAttributesMapper(Properties properties)
+	{
+		this.properties = properties;
+	}
+	
 	public Object mapFromAttributes(Attributes attributes) throws NamingException
 	{
-		return null;
+		// create a group resource
+		Group group = ResourceUtilities.FACTORY.createGroup();
+		
+		// get the gid attribute name
+		String gidAtttributeName = DEFAULT_GID_ATTRIBUTE;
+		if(properties.containsKey(GID_ATTRIBUTE)) gidAtttributeName = properties.getProperty(GID_ATTRIBUTE);
+		
+		// get the gid		
+		Attribute gidAttribute = attributes.get(gidAtttributeName);
+		if(gidAttribute != null) group.setId((String)gidAttribute.get());
+		
+		// get the member attribute name
+		String memberAtttributeName = DEFAULT_MEMBER_ATTRIBUTE;
+		if(properties.containsKey(MEMBER_ATTRIBUTE)) memberAtttributeName = properties.getProperty(MEMBER_ATTRIBUTE);
+		
+		// get the members
+		NamingEnumeration memberEnumeration = attributes.get(memberAtttributeName).getAll();
+		if(memberEnumeration != null)
+		{
+			// create a members resource
+			List<PluralAttribute> members = new ArrayList<PluralAttribute>();
+			
+			while(memberEnumeration.hasMoreElements())
+			{
+				// get the next member
+				String memberAttribute = (String)memberEnumeration.next();
+				if(memberEnumeration != null)
+				{				
+					PluralAttribute pluralAttribute = ResourceUtilities.FACTORY.createPluralAttribute();
+					pluralAttribute.setValue(memberAttribute);
+					
+					members.add(pluralAttribute);
+				}
+			}
+			
+			// add the members to the group resource
+			group.setAny(members);
+		}
+		
+		return group;
      }
 }

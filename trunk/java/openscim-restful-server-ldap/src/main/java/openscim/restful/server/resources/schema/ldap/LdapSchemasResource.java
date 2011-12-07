f288a8e6e625555b5f2eb06d3dbc77b1b068856e
matthewcrooke@gmail.com <matthewcrooke@gmail.com@94e669b5-fa3f-8ef2-5a11-f9e0e53556cc>
2011-12-07 11:59:41 +0000

    Split ldap datasource into extension package

---
+++ trunk/java/openscim-restful-server-ldap/src/main/java/openscim/restful/server/resources/schema/ldap/LdapSchemasResource.java
@@ -0,0 +1,51 @@
+/*
+ * openscim restful scim server
+ * http://code.google.com/p/openscim/
+ * Copyright (C) 2011 Matthew Crooke <matthewcrooke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package openscim.restful.server.resources.schema.ldap;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import openscim.restful.server.resources.schema.SchemasResource;
+import openscim.restful.server.resources.util.ResourceUtilities;
+
+import org.apache.log4j.Logger;
+import org.apache.wink.common.http.HttpStatus;
+import org.springframework.ldap.core.LdapTemplate;
+
+
+public class LdapSchemasResource extends SchemasResource
+{
+	// log4j contants
+	private static Logger logger = Logger.getLogger(LdapSchemasResource.class);
+	
+	// ldap parameters
+	private LdapTemplate ldapTemplate = null;
+	 
+	public void setLdapTemplate(LdapTemplate ldapTemplate)
+	{
+		this.ldapTemplate = ldapTemplate;
+	}
+	
+	@Override
+	public Response listSchemas(UriInfo uriInfo, String resource)
+	{
+		// return an operation unsupported response
+		return ResourceUtilities.buildErrorResponse(HttpStatus.NOT_IMPLEMENTED, HttpStatus.NOT_IMPLEMENTED.getMessage() + ": Service Provider does not support the list schemas operation");
+	}
+}
